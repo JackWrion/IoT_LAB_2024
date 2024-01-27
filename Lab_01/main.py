3aada189760a9564f9a053ba04662998b402fc75
jackwr <jackwrion12345@gmail.com>
2024-01-27 15:39:07 +0700

    fix in linux

--- Lab_01/main.py
+++ Lab_01/main.py
@@ -6,33 +6,9 @@ from numpy import random
 
 
 #1.-----config login Ada
-AIO_USERNAME = "jackwrion12345"
-AIO_KEY = "YWlvX1VoS202M3FVa0FBTmNrT2FMZUZLWkZtR0NGOVM="
+AIO_USERNAME = "jackwr"
+AIO_KEY = "YWlvX2pwa1IyNVFXUllRZTYzRFR5T0U3RWtMY1ZDc2g="
 
-
-#2.-----Connect Microbit
-def getPort () :
-    ports = serial.tools.list_ports.comports()
-    N = len(ports)
-    commPort = "None"
-    for i in range (0 , N) :
-        port = ports [ i ]
-        strPort = str( port )
-        if "USB-SERIAL CH340" in strPort :
-            splitPort = strPort.split (" ")
-            commPort = ( splitPort [0])
-    return commPort
-
-def ConnectPort():
-    port = getPort()
-    if (port == 'None'): return
-    return serial.Serial(port , baudrate =115200)
-
-
-def accessPort():
-    global ser
-    ser = ConnectPort()
-    print('Connecting Serial: ' , ser)
 
 
 
@@ -50,6 +26,8 @@ def subscribe ( client , userdata , mid , granted_qos ) :
 def disconnected ( client ) :
     print("Ngat ket noi ...")
     sys.exit(1)
+
+
 
 
 
@@ -77,74 +55,15 @@ def message ( client , feed_id , payload ):
         
         
         
-    
-
-
-
-#readSerial from Microbit --> processData --> publish to Adafruit
-#Get message from Adafruit --> ser.write to Microbit
-
-
-
-###################################################################################
-######################    Send from Microbit to Gateway   #########################
-
-
-# Read from Microbit
-
-#Function about Reading process
-def processData ( data ) :
-    data = data.replace ("!", "")
-    data = data.replace ("#", "")
-    splitData = data.split(":")
-    print ( splitData )
-    if splitData[1] == "TEMP":
-        client.publish("bbc-temp", splitData[2])
-
-
-mess = ""
-def readSerial():
-    bytesToRead = ser.inWaiting()
-    if (bytesToRead > 0):
-        global mess
-        mess = mess + ser.read(bytesToRead).decode("UTF-8")
-        while ("#" in mess) and ("!" in mess):
-            start = mess.find("!")
-            end = mess.find("#")
-            processData(mess[start:end + 1])
-            if (end == len(mess)):
-                mess = ""
-            else:
-                mess = mess[end+1:]
-
-
-###################################################################################
-
-
-
-
-
-
-# Connect to Ada
-
-AIO_KEY = (base64.b64decode(AIO_KEY.encode("utf-8"))).decode("utf-8")
-client = MQTTClient ( AIO_USERNAME , AIO_KEY )
-client.on_connect = connected
-client.on_disconnect = disconnected
-client.on_message = message
-client.on_subscribe = subscribe
-client.connect()
-client.loop_background()
-
-
-
-# API
-# Open door
-# Close door
+        
+        
+# Connect to AdaFRUIT
 
 
 
 def ConnectAdafruit():
+    global AIO_KEY
+    AIO_KEY = (base64.b64decode(AIO_KEY.encode("utf-8"))).decode("utf-8")
     client = MQTTClient ( AIO_USERNAME , AIO_KEY )
     client.on_connect = connected
     client.on_disconnect = disconnected
