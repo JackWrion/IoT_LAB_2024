a88e488971dcaa0a69a8f1e9b8d24f27c2e78eb0
jackwr <jackwrion12345@gmail.com>
2024-02-21 18:49:23 +0700

    release: stable MQTT class

--- Lab_02/MQTT_driver.py
+++ Lab_02/MQTT_driver.py
@@ -2,74 +2,28 @@ import sys
 from Adafruit_IO import MQTTClient, Client, Feed
 import time
 import base64
-from numpy import random
 
-from AI_driver import AI_driver
-from UART_driver import *
-
-#1.-----config login Ada
-AIO_USERNAME = "jackwr"
-AIO_KEY = "YWlvX2pwa1IyNVFXUllRZTYzRFR5T0U3RWtMY1ZDc2g="
-AIO_KEY = (base64.b64decode(AIO_KEY.encode("utf-8"))).decode("utf-8")
-#3.------Connect Adafruit
-#Function about Adafruit
+from Logger import *
 
 
-
-
-
-
-def message ( client , feed_id , payload ):
-    
-    if (feed_id == "led"):
-        if payload == "1":
-            print("LED ON !!!")
-        else:
-            print("LED OFF #")
-                
-    elif (feed_id == "water-heater"):
-        if payload == "1":
-            print("WATER HEATER ON !!!")
-        else:
-            print("WATER HEATER OFF ###")
-
-    elif (feed_id == "air-conditioner"):
-        global temp_offset
-        print (f"Setting Air Conditioner at {payload} ºC")
-        temp_offset = float(payload) + 0.5
-    
-    elif (feed_id == "humi"):
-        global humi_offset
-        print (f"Reading Humidity: {payload}%")
-        humi_offset = float(payload) + 0.5
-    else:
-        print ("UNKNOWN COMMAND !!!")
-        
-        
-        
-        
-        
 # Connect to AdaFRUIT
 
 class MQTT_Client:
 
+    def pseudo_on_message(self, client , feed_id , payload):
+        print_warning("MQTT: DO NOTHING", feed_id, payload)
 
     def connected (self, client):
-        print("\033[32mMQTT: CONNECTED SUCCESSFULLY !!!")
+        print_log("MQTT: CONNECTED SUCCESSFULLY !!!")
         print(client)
-        # self.client.subscribe( "air-conditioner" )
-        # self.subscribe( "led" )
-        # self.subscribe( "water-heater" )
-        # self.subscribe( "humi" )
-
-
+        
 
     def subscribe (self, client , userdata , mid , granted_qos ):
-        print("\033[32mMQTT: SUBSCRIBE SUCCESSFULLY ---> ", self.reg_topic)
+        print_log("MQTT: SUBSCRIBE SUCCESSFULLY > TOPIC:", self.reg_topic)
 
 
     def disconnected (self, client ) :
-        print("\033[31mMQTT: DISCONNECTED ...")
+        print_warning("MQTT: DISCONNECTED ...")
         sys.exit(1)
 
 
@@ -77,10 +31,13 @@ class MQTT_Client:
         self.client = MQTTClient ( username , password )
         self.client.on_connect = self.connected
         self.client.on_disconnect = self.disconnected
-        #self.client.on_message = self.message
+        self.client.on_message = self.pseudo_on_message
         self.client.on_subscribe = self.subscribe
         self.client.connect()
         self.client.loop_background()
+
+
+
 
 
     def MQTT_Subscribe(self, listofTopics):
@@ -88,80 +45,62 @@ class MQTT_Client:
 
         if isinstance(listofTopics, str):
             self.reg_topic = listofTopics
-            self.client.subscribe(topic)
+            self.client.subscribe(listofTopics)
         elif isinstance(listofTopics, tuple):
             for topic in listofTopics:
                 self.reg_topic = topic
                 self.client.subscribe(topic)
 
 
-    def MQTT_RegisMessagesProcess(self, func):
+    def MQTT_RegMessagesEvent(self, func):
+        print_warning("MQTT: REGISTERED NEW EVENT CALLBACK !!!")
         self.client.on_message = func
-        # self.client.disconnect()
-        # self.client.connect()
-        # self.MQTT_Subscribe(self.listofTopics)
-
-
-
-
-
-
-
-# Initialization
-
-client_1 = MQTT_Client(AIO_USERNAME, AIO_KEY)
-
-
-ConnectPort("/dev/pts/1")
-battery = 70.0
-bat_offset = 0.2
-temp_offset = 25
-humi_offset = 65
-
-sensor_turn = 0
-
-
-
-while True:
-    readSerial(client=client_1)
-
-    # Begin MAIN_FLOW    
-    if sensor_turn == 0:
-        offset = round(random.randn(),2)
-        temp =  float (temp_offset + offset)
-        client_1.publish("temp", temp)
-        print("Temperature: ",temp)
-        sensor_turn = 1
     
-    elif sensor_turn == 1:
-        offset = round(random.randn(),2)
-        humi =  float (humi_offset - offset)
-        client_1.publish("humi", humi)
-        print("Humidity: ",humi)
-        sensor_turn = 2
-    
-    elif sensor_turn == 2:
-        battery = round(battery - bat_offset,2)
-        if battery < 1:
-            bat_offset = -0.2
-        elif battery > 100:
-            battery = 100
-            bat_offset = 0.2
-        client_1.publish("battery", battery)
-        print("Battery: ",battery)
-        sensor_turn = 3
-    
-    elif sensor_turn == 3:
-        data, class_name = AI_driver.AI_Execute()
-        client_1.publish("camera", data)
-        print("Camera detect: ", class_name)
-        sensor_turn = 0
+    def MQTT_Publish(self, topic, data):
+        self.client.publish(topic, data)
+        print_log("MQTT: PUBLISH TO",topic,"=",data)
 
 
 
-    # End MAIN_FLOW
-    time.sleep(2)
+
+
+class MQTT_Sensors:
+    battery = 70.0
+    temp = 25
+    humi = 65
+
+    humi_offset = 0
+    temp_offset = 0
+    bat_offset = 0.2
 
 
 
-AI_driver.AI_Stop()
+
+
+
+
+
+
+# #----FOR TESTING ONLY ---#
+# #1.-----config login Ada infomation
+# AIO_USERNAME = "jackwr"
+# AIO_KEY = "YWlvX2pwa1IyNVFXUllRZTYzRFR5T0U3RWtMY1ZDc2g="
+# AIO_KEY = (base64.b64decode(AIO_KEY.encode("utf-8"))).decode("utf-8")
+
+# #2.-----init client and sensors
+# client_1 = MQTT_Client(AIO_USERNAME, AIO_KEY)
+# client_1.MQTT_Subscribe("temp")
+
+# mysensor = MQTT_Sensors()
+# #3.-----loop processing
+# turn = 0
+
+# def myEvent(client , feed_id , payload):
+#     print("TESTING NEW EVENT SUCCESSFULLY !!!")
+
+# while True:
+#     client_1.MQTT_Publish("temp",mysensor.temp)
+#     if turn == 3:
+#         client_1.MQTT_RegMessagesEvent(myEvent)
+#     turn += 1
+#     time.sleep(2)
